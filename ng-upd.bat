39e5b7e39bcc1e820f45485a47562c125d7a349f
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2019-05-30 14:45:48 -0500

    npm audit fix

--- ng-upd.bat
+++ ng-upd.bat
@@ -5,3 +5,4 @@ REM CALL ng update @angular-devkit/build-angular
 REM CALL ng update @angular-devkit/architect
 REM CALL ng update @angular-devkit/core
 REM CALL ng update @angular-devkit/schematics
+CALL npm audit fix
