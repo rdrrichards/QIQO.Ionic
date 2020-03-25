bd289f6c3d4a23d6b8df77d713d6495ce0879b5e
Richard Richards <rdrrichards@hotmail.com>
2020-03-25 15:43:15 -0500

    misc update commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -3,6 +3,8 @@ CALL ng update @angular/core --force
 CALL git commit -am "ng update to latest"
 CALL ng update @angular/cli --force
 CALL git commit -am "cli update to latest"
+CALL ng update @angular-devkit/architect
+CALL git commit -am "angular-devkit/architect update to latest"
 CALL npm audit fix
 CALL git commit -am "post update commit"
 CALL bt.bat
